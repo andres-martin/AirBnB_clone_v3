96cb5f97edb6b8beb4ced310558b2e8bccf636a8
yawzyag <yesid.yag@gmail.com>
2019-09-13 17:16:08 +0000

    [Fix] to dict

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -30,6 +30,7 @@ def place_search():
     """get City with his id"""
     list_places = []
     list_place = []
+    list_final = []
 
     if request.is_json:
         data = request.get_json()
@@ -41,20 +42,20 @@ def place_search():
         list_place.append(val)
     if len(data) == 0:
         for val in storage.all("Place").values():
-            list_places.append(val.to_dict())
+            list_places.append(val)
     else:
         if "states" in data and len(data["states"]) > 0:
             for state_id in data["states"]:
                 place = storage.get("State", state_id)
                 for city in place.cities:
                     for pla in city.places:
-                        list_places.append(pla.to_dict())
+                        list_places.append(pla)
 
         if "cities" in data and len(data["cities"]) > 0:
             for city_id in data["cities"]:
                 city = storage.get("City", city_id)
                 for pla in city.places:
-                    list_places.append(pla.to_dict())
+                    list_places.append(pla)
 
         if "amenities" in data and len(data["amenities"]) > 0:
             amenities = set(list(a_id for a_id in data["amenities"]
@@ -75,9 +76,17 @@ def place_search():
 
                     if "amenities" in llave:
                         del llave["amenities"]
-                    list_places.append(llave)
+                    list_places.append(place)
 
-    return jsonify(list_places)
+    
+
+    for pl in list_places:
+        llave = pl.to_dict()
+        if "amenities" in llave:
+            del llave["amenities"]
+        list_final.append(llave)
+
+    return jsonify(list_final)
 
 
 @app_views.route('/places/<place_id>', methods=['DELETE'])
